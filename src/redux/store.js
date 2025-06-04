894f694fd7ac188a0722255b5a724827140df9b4
Chekan <ky.chekan@gmail.com>
2025-06-04 14:41:01 +0300

    create contactOps

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactsReducer from "./contactsSlice.js";
 import filtersReducer from "./filtersSlice.js";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
 
-const persistConfig = {
-  key: "contacts",
-  storage: storage,
-};
-
-const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
-
-const store = configureStore({
+export const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer,
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
 });
-
-const persistor = persistStore(store);
-
-export { store, persistor };
